db68c3e885ab36b23f2931f641d3efbdf6d76147
jcmann <jen.c.mann@gmail.com>
2021-03-15 22:39:58 -0500

    Restructured to call a getLatLon method which collects zipcode's lat/long and area name.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = () => {
     const getWeatherButton = document.querySelector("#getWeather"); 
 
     // Event listener for getWeather onclick 
-    getWeather.addEventListener("click", event => {
+    getWeatherButton.addEventListener("click", event => {
 
         // Get the zipcode inputted from user
         let zipcode = zipcodeInput.value; 
@@ -12,8 +12,12 @@ window.onload = () => {
         // Form the request URL 
         let zipcodeURL = createURL(zipcode); 
 
-        // (method, url, callback)
-        ajaxRequest("get", zipcodeURL, getLatLon); 
+        let xhr = new XMLHttpRequest(); 
+
+        // open XHR for request 1: get lat/lon 
+        getLatLon(xhr, zipcodeURL); 
+
+        // open XHR for request 2: actual weather data
         
     });
 
@@ -28,27 +32,19 @@ window.onload = () => {
 
     }
 
-    /**
-     * A general ajax request to handle both requests made for this app.
-     */
-    const ajaxRequest = (method, url, callback) => {
-
-        // Create and use XHR 
-        let xhr = new XMLHttpRequest(); 
-        //console.log(xhr);
-        xhr.open(method, url); // We are only using get for this app
+    const getLatLon = (xhr, zipcodeURL) => {
+        
+        xhr.open("get", zipcodeURL); // We are only using get for this app
 
         xhr.onreadystatechange = () => {
             if (xhr.readyState == 4) {
-                
+                // We are using the JSON request, so all responses should be JSON
+                console.log(xhr.responseText);
             }
         }
 
         xhr.send(null); 
-    }
 
-    const getLatLon = (response) => {
-        console.log(response);
     }
 
 
